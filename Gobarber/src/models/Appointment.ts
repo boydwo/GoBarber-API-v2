ffad2118fe50c4cc22f59024adea6db091b5a562
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-02-18 14:11:56 -0300

    repositories, DTO and OMIT ts

--- Gobarber/src/models/Appointment.ts
+++ Gobarber/src/models/Appointment.ts
@@ -2,14 +2,16 @@ import { v4 as uuid } from 'uuid';
 
 class Appointment {
   id: string;
+
   provider: string;
+
   date: Date;
 
-   constructor(provider: string, date: Date){
-     this.id = uuid();
-     this.provider = provider;
-     this.date = date;
-   }
+  constructor({ provider, date }: Omit<Appointment, 'id'>) {
+    this.id = uuid();
+    this.provider = provider;
+    this.date = date;
+  }
 }
 
 export default Appointment;
--- Gobarber/src/routes/appointments.routes.ts
+++ Gobarber/src/routes/appointments.routes.ts
@@ -1,28 +1,38 @@
-import {  Router } from 'express'
-import {startOfHour, parseISO, isEqual} from 'date-fns';
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments: Appointment[]=[];
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
 
-appointmentsRouter.post('/', (request, response)=>{
-  const {provider, date} = request.body;
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  
-  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date))
 
-  if(findAppointmentInSameDate){
-    return response.status(400).json({message: 'This appointment is already booked'});
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
   }
-  const appointment = new Appointment(provider, parsedDate);
 
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
-
-})
+});
 
 export default appointmentsRouter;
