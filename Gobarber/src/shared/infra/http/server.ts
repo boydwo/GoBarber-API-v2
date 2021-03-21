872f84866cf1126278398e2b3e4ca5017dbd1522
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-03-21 12:36:00 -0300

    email provider with nodemailer and handlebars

--- Gobarber/src/shared/infra/http/server.ts
+++ Gobarber/src/shared/infra/http/server.ts
@@ -12,7 +12,7 @@ import '@shared/container';
 
 const app = express();
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use(
