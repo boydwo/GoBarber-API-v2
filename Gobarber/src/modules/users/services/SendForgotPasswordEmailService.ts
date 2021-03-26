1eadb6463a947a407d1d33799909878b645e43ac
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-03-26 09:38:24 -0300

    template engine email

--- Gobarber/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ Gobarber/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 
@@ -34,14 +35,21 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: { name: user.name, email: user.email },
       subject: 'Recuperação de senha',
       templateData: {
-        template: 'Olá, {{name}} : {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
