2f5330af7f2536375d1240ea94b3c053ac810253
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-04-01 10:50:45 -0300

    appointments provider final

--- Gobarber/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ Gobarber/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -3,12 +3,24 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import ProviderController from '../controllers/ProviderController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
 
 const providersRouter = Router();
 const providerController = new ProviderController();
+const providerDayAvailability = new ProviderDayAvailabilityController();
+const providerMonthAvailability = new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providerController.index);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailability.index,
+);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailability.index,
+);
 
 export default providersRouter;
--- Gobarber/src/modules/appointments/services/CreateAppointmentService.ts
+++ Gobarber/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -30,10 +30,23 @@ class CreateAppointmentService {
       appointmentDate,
     );
 
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date.");
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself.");
+    }
+
     if (findAppointmentInSameDate) {
       throw new AppError('this appointment is already booked');
     }
 
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create appointments between 8am and 5am',
+      );
+    }
     const appointment = this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
